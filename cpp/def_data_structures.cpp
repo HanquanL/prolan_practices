249a579d78ce9740548d8cc524a3eee372ca228b
HanquanL <Hanquanl0115@gmail.com>
2026-05-11 10:37:34 -0400

    def_data_structures.cpp

---
+++ cpp/def_data_structures.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    double price = 3;
+
+    struct Sales_data{
+        string bookNo;
+        unsigned units_sold = 0;
+        double revenue = 0.0;
+    };
+
+    Sales_data my_data;
+    my_data.bookNo = "0-201-78345-X";
+    my_data.units_sold = 6;
+    my_data.revenue = my_data.units_sold * price;
+
+    cout << "bookNo: " << my_data.bookNo << endl;
+    cout << "unit_sold: " << my_data.units_sold << endl;
+    cout << "revenue: " << my_data.revenue << endl;
+
+}
