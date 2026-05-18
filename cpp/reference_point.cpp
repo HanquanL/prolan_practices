c27d7831a55a9eb49a9f73b915b823fb4ddd8c63
HanquanL <Hanquanl0115@gmail.com>
2026-05-18 11:36:15 -0400

    revised sales_data.h

--- cpp/reference_point.cpp
+++ cpp/reference_point.cpp
@@ -6,11 +6,12 @@ int main(){
     int i = 1024;
     int &ref_i = i;
     int *p_i = &ref_i;
-    cout << ref_i << endl;
-    cout << &ref_i << endl;
-    cout << &i << endl;
-    cout << *p_i << endl;
-    cout << p_i << endl;
+    cout << ref_i << endl;  //1024
+    cout << &ref_i << endl; //0x61ff04
+    cout << &i << endl; //0x61ff04
+    cout << *p_i << endl; //1024
+    cout << p_i << endl; //0x61ff04
+    cout << &p_i << endl; //0x61ff08
     ref_i = 2048;
     cout << "ref_i was changed to: " << ref_i << " and its address now: " << &ref_i << endl;
 }
--- cpp/sales_data.h
+++ cpp/sales_data.h
@@ -4,9 +4,29 @@
 #include <string>
 using namespace std;
 
+double Sales_data::avg_price() const {
+    if(units_sold){
+        return revenue / units_sold;
+    }else{
+        return 0;
+    }
+}
+
+Sales_data& Sales_data::combine(const Sales_data &rhs){
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+
+}
+
 struct Sales_data{
         string bookNo;
         unsigned units_sold = 0;
         double revenue = 0.0;
+
+        string isbn() {return bookNo;}
+        Sales_data& combine (const Sales_data&);
+        double avg_price() const;
+
     };
 #endif
