b25655a367c6828302da12e964599af3d7d91abf
HanquanL <Hanquanl0115@gmail.com>
2026-06-04 21:26:47 -0400

    done monitor_obj_pattern.cpp

--- os/monitor_obj_pattern.cpp
+++ os/monitor_obj_pattern.cpp
@@ -17,24 +17,45 @@ class WaitingVehicles{
     private:
         std::vector<Vehicle> _vehicles;
         std::mutex _mtx;
+        int _numVehicles;
     public:
         WaitingVehicles() {};
 
-        void printIDs(){
-            std::lock_guard<std::mutex> mylock(_mtx);      // lock is release when mylock goes out of scope
-            for(auto &v : _vehicles){
-                std::cout << " Vehicle #" << v.getID() << " is now waiting in the queue" << std::endl;
-            }
+        int getNumVehicles(){
+            std::lock_guard<std::mutex> ulock(_mtx);
+            return _numVehicles;
+        }
+
+        bool dataIsAvailable(){
+            std::lock_guard<std::mutex> mylock(_mtx);
+            return !_vehicles.empty();
+        }
+        // void printIDs(){
+        //     std::lock_guard<std::mutex> mylock(_mtx);      // lock is release when mylock goes out of scope
+        //     for(auto &v : _vehicles){
+        //         std::cout << " Vehicle #" << v.getID() << " is now waiting in the queue" << std::endl;
+        //     }
+        // }
+        Vehicle popBack(){
+            // perform vector modification under the lock
+            std::lock_guard<std::mutex> ulock(_mtx);
+            // remove last vector element from queue
+            Vehicle v = std::move(_vehicles.back());
+            _vehicles.pop_back();
+            --_numVehicles;
+            return v;    // will not be copied due to return value optimization (RVO) in c++
         }
 
         void pushback(Vehicle &&v){
+            // simulate some work
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
             //perform vector modification under the lock
             std::lock_guard<std::mutex> ulock(_mtx);
             std::cout << "   Vehicle #" << v.getID() << " will be added to the queue" << std::endl; 
             _vehicles.emplace_back(std::move(v));
-
+            ++_numVehicles;
             // simulate some work
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            // std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
 };
 
@@ -50,12 +71,26 @@ int main(){
         futures.emplace_back(std::async(std::launch::async, &WaitingVehicles::pushback, queue, std::move(v)));
     }
 
+
+    std::cout << "ollecting results...." << std::endl;
+    // queue -> printIDs();
+    while(true){
+        if(queue -> dataIsAvailable()){
+            Vehicle v = queue -> popBack();
+            std::cout << " Vehicle #" << v.getID() << " has been removed from the queue" << std::endl;
+
+            if(queue -> getNumVehicles() <= 0){
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                break;
+            }
+        }
+    }
+
     std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr){
         ftr.wait();
     });
 
-    std::cout << "Vollecting results...." << std::endl;
-    queue -> printIDs();
+    std::cout << "Finished : " << queue->getNumVehicles() << " vehicle(s) left in the queue" << std::endl;
 
     return 0;
 }
